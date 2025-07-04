e8ab0033593a22e00dd5afec7e9913b41605e88f
Anderson <andersonastudillo2004@gmail.com>
2025-07-04 10:36:53 -0500

    Agregar modelo para Capitanes

--- mapa/models.py
+++ mapa/models.py
@@ -2,3 +2,9 @@ from django.db import models
 
 # Create your models here.
 
+class Capitan(models.Model): 
+    nombre = models.CharField(max_length=100)
+    fecha_registro = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.nombre
